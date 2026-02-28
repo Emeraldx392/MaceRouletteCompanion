8623b6a9b6c561a67c28c868a9ef883ab434e4f7
Emeraldx392 <emeraldx3922.0@gmail.com>
2026-02-28 13:41:09 +0100

    Crash fix

--- src/main/kotlin/moe/pxe/macecompanion/enums/HudElements.kt
+++ src/main/kotlin/moe/pxe/macecompanion/enums/HudElements.kt
@@ -210,7 +210,7 @@ enum class HudElements : NameableEnum, StringIdentifiable, ConfigurableEnum {
             val textRenderer = MinecraftClient.getInstance().textRenderer
             val text = Text.translatable("mrc.roundhud.mace_chance_text",
                 Text.literal("%.2f%%".format(StateManager.maceChance)).also {
-                    if (Config.chanceUseColor.value) it.setStyle(Config.getAccentStyle(textColors[floor(StateManager.maceChance / 7.69).toInt()]))
+                    if (Config.chanceUseColor.value) it.setStyle(Config.getAccentStyle(textColors[floor(StateManager.maceChance / 7.7).toInt()]))
                 }).setStyle(Config.getAccentStyle(0x79fc00))
 
             val width = textRenderer.getWidth(text)
